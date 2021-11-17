d4878b80bbf35be49170646e37f05b10b6f1a24a
SifatNiloy <sifatniloy18@gmail.com>
2021-11-17 23:23:27 +0600

    done something

--- book.js
+++ book.js
@@ -1,1 +1,21 @@
 console.log('I got that');
+
+// searchText();
+const loadBookData = () => {
+
+    const inputText = document.getElementById('text-input');
+    const searchText = inputText.value;
+    console.log(searchText);
+
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            const displayNumberFound = data.numFound;
+            const Searchdata = data.docs;
+            console.log(Searchdata);
+        })
+
+}
+loadBookData();
