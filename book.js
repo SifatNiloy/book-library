061f034b3330b0091e836d6dc0cfc802b1d605ed
SifatNiloy <sifatniloy18@gmail.com>
2021-11-22 00:08:58 +0600

    push complete

--- book.js
+++ book.js
@@ -16,6 +16,7 @@ const loadBookData = () => {
     if (searchText === '') {
         availableBooks(`please enter book name`);
     }
+    
     else {
         console.log(searchText);
         const url = `https://openlibrary.org/search.json?q=${searchText}`;
