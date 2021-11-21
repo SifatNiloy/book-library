a0d3b56b7fb2845a18210f7329a39c53a5657273
SifatNiloy <sifatniloy18@gmail.com>
2021-11-21 23:05:13 +0600

    main things done

--- book.js
+++ book.js
@@ -5,13 +5,13 @@ const availableBooks = text => {
     h2.innerHTML = text;
     bookFound.appendChild(h2);
 }
-
 const loadBookData = () => {
+    const searchResult = document.getElementById('search-results');
+    searchResult.textContent = '';
 
     const inputText = document.getElementById('text-input');
     const searchText = inputText.value;
     if (searchText == '') {
-
         availableBooks(`please enter book name`);
     }
     else {
@@ -20,15 +20,11 @@ const loadBookData = () => {
         fetch(url)
             .then(res => res.json())
             .then(data => displaySearchResult(data))
-
     }
     inputText.value = '';
-
 }
 // loadBookData();
 const displaySearchResult = data => {
-
-    console.log(data.docs);
     const books = data.docs;
     // console.log(books);
     if (books.length == 0) {
@@ -38,32 +34,30 @@ const displaySearchResult = data => {
     else {
 
         availableBooks(`Total books found: ${data.numFound}`)
-    }
-    const searchResult = document.getElementById('search-results');
-    searchResult.textContent = '';
+        const searchResult = document.getElementById('search-results');
+        searchResult.textContent = '';
 
-    console.log(data.numFound);
-    books.forEach(book => {
-        // console.log(book.title);
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
-                    
-            <div class="col">
-              <div class="card" >
-                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">${book.title}</h5>
-                  <h4 class="card-text"> Author:${book.author_name} </h4>
-                  <p class="card-text"> First publish year:${book.first_publish_year} </p>
-                  <p class="card-text"> Publisher:${book.publisher} </p>
-                  <p class="card-text"></p>
+        console.log(data.numFound);
+        books.forEach(book => {
+            // console.log(book.title);
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+                        
+                <div class="col">
+                  <div class="card" >
+                    <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">${book.title}</h5>
+                      <h4 class="card-text"> Author:${book.author_name} </h4>
+                      <p class="card-text"> First publish year:${book.first_publish_year} </p>
+                      <p class="card-text"> Publisher:${book.publisher} </p>
+                      <p class="card-text"></p>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-            `
-        searchResult.appendChild(div);
-    })
-
-
+                `
+            searchResult.appendChild(div);
+        })
+    }
 }
